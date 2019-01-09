58910f8c1e96ea593c877aecab91d3518d714ac2
nha5861 <nha5861@users.noreply.github.com>
2019-01-09 18:22:40 +0700

    Create tinh cv,dt htron.cpp

---
+++ tinh cv,dt htron.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#define PI: 3.14
+using namespace std;
+
+int main()
+{
+	double r;
+	double p;
+	double s;
+	cout<<"please input r: ";
+	cin>>r;
+	p=(r*2)*PI;
+	s=r*r*PI;
+	cout<<"p: "<<p<<endl;
+	cout<<"s: "<<s<<endl;
+	
+	return 0;
+}
